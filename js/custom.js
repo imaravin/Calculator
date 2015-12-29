b719201a4c5f083bf6bdd95ab7c8a1044112a046
imaravin <aravinththangasami@gmail.com>
2015-12-30 01:27:38 +0530

    calculator

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,17 @@
 $(document).ready(function () {
 	$('.btn').click(function(event){
 		if($(this).html()==='=')
-			$('.jumbotron').text(cal($('.jumbotron').html))
+			$('.jumbotron').text(cal($('.jumbotron').html()))
 		else
 		$('.jumbotron').text($('.jumbotron').html()+$(this).html());
 	});
 });
 function cal(x)
 {
-	return "aaa"
+	var str=String(x);
+	var arr=str.split("+");
+	if (arr.length==2)
+	return parseFloat(arr[0])+parseFloat(arr[1]);
+	else
+		return "a";
 }
