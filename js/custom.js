1a8a5b81f3f47fdce79d684acad3fba6cb42d806
imaravin <aravinththangasami@gmail.com>
2015-12-30 12:57:39 +0530

    functions

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,44 @@
+var error=false;
+var ans=false;
+
 $(document).ready(function () {
 	$('.btn').click(function(event){
-		if($(this).html()==='=')
+
+		if(ans)
+		{
+			$('.jumbotron').text('');
+		}
+		
+		if(error){
+			$('.jumbotron').text($(this).html());
+			error=false;
+		}
+		else if($(this).html()==='='){
 			$('.jumbotron').text(cal($('.jumbotron').html()))
+			//cal=true;
+		}
 		else
 		$('.jumbotron').text($('.jumbotron').html()+$(this).html());
 	});
 });
 function cal(x)
 {
+	ans=true;
 	var str=String(x);
 	var arr=str.split("+");
 	if (arr.length==2)
 	return parseFloat(arr[0])+parseFloat(arr[1]);
-	else
-		return "a";
+
+	arr=str.split("-");
+		if (arr.length==2)
+			return parseFloat(arr[0])-parseFloat(arr[1]);
+
+   arr=str.split("*");
+		if (arr.length==2)
+			return parseFloat(arr[0])*parseFloat(arr[1]);
+	 arr=str.split("/");
+		if (arr.length==2)
+		return parseFloat(arr[0])/parseFloat(arr[1]);
+
+	return "Error"
 }
